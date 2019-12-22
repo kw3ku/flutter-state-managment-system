85a52fbd804470c4a3461aaab63292de6b4e2f15
Kwaku Dompreh <kwekugt@gmail.com>
2019-12-22 19:01:45 +0000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html class="no-js">
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" type="image/png" href="images/favicon.png">
+    <title>OXGLOW ANALYTICS - Thesis and Dissertation Writing in Ghana</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="css/slider.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/responsive.css">
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/oxglowbase.min.js"></script>
+    <meta name="description" content="">
+    <script src="js/main.js"></script>
+    <script src="js/slider.js"></script>
+        <script src="js/jquery.fancybox.js"></script>
+        
+</head>
+<body>
+    <!-- Header get Started // -->
+
+    <header id="oxtop-bar" class="oxbar-fixed-top animated-header"> 
+                <div class="oxcontain">
+            <div class="oxbar-header">
+
+            <button type="button" class="oxbar-toggle" data-toggle="collapse" data-target=".oxbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+
+
+                <!-- logo -->
+                <div class="oxbar-brand">
+                    <a href="#">
+                        <img src="images/logo.png" alt="" width="80" height="60">
+                                </a>
+                                </div>
+                                </div>
+                                <!-- /logo -->
+
+                                <!-- main menu -->
+                                <nav class="collapse oxbar-collapse oxbar-right" role="navigation">
+                                               <div class="main-menu">
+                                                   
+                                                   <ul class="nav oxbar-nav oxbar-right">
+                                                       <li> 
+                                                           <a href="#" > Students </a>
+                                                            </li>
+                                                    <li> 
+                                                        <a href="#"> Corporate </a> 
+                                                    </li>
+                                                    <li class="dropdown">
+                                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"> Resource <span class="caret"></span> </a>
+                                            
+                                                    <div class="dropdown-menu">
+                                                        <ul>
+                                                            <li><a href="#"> Plagiarism </a></li>
+                                                            <li><a href="#"> Data Tools </a></li>
+                                                        </ul>
+                                                        </div>
+                                                        </li>
+                                                            <li><a href="blog.html">blog</a></li>
+                                                                <li><a href="contact.php">Contact us</a></li>
+                                                                </ul>
+                                                                </div>
+                                                            </nav>
+                                                            </div>
+                                                            </header>
+
+    
